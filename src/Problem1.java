e457084a5a919f1aa3a87fe08857efec76c73ee0
tmi <michelet.tristan@gmail.com>
2013-01-07 18:43:36 +0100

    cleaning of problem 1

--- src/Problem1.java
+++ src/Problem1.java
@@ -17,7 +17,32 @@ public class Problem1 {
 		assertEquals(14, natural35Sum(9));
 		assertEquals(33, natural35Sum(11));
 		assertEquals(233168, natural35Sum(1000));
-		
+	}
+
+	
+	// easy case with 3 and 5
+	private int natural35Sum(int maximumNumber) {
+		int sum = 0;
+		for (int i = 0; i < maximumNumber; i++) {
+			if(shouldWeAddCurrentInteger(i)) {
+				sum+= i;
+			}
+		}
+		return sum;
+	}
+
+	private boolean shouldWeAddCurrentInteger(int i) {
+		return i%3 == 0 || i%5 == 0;
+	}
+	
+	
+
+	
+	
+	// to go further
+	
+	@Test
+	public void furtherTest() {
 		int[] multiples = new int[]{3,5};
 		assertEquals(23, naturalSum(10, multiples));
 		assertEquals(14, naturalSum(9, multiples));
@@ -25,35 +50,25 @@ public class Problem1 {
 		assertEquals(233168, naturalSum(1000, multiples));
 	}
 
-	// more global case
-	private int naturalSum(int number, int[] multiples) {
+	
+	private int naturalSum(int maximumNumber, int[] multiplesToConsider) {
 		int sum = 0;
-		boolean b;
 		
-		for (int currentNumber = 0; currentNumber < number; currentNumber++) {
-			b = false;
-			for (int currentMultiple : multiples) {
-				if (currentNumber % currentMultiple == 0) { // if at least one is true, we add currentNumber to sum
-					b = true;
-				}
-			}
-			if(b) {
-				sum+= currentNumber;
-			}
-		}
-		return sum;
-	}
-	
-	
-	// easy case with 3 and 5
-	private int natural35Sum(int number) {
-		int sum = 0;
-		for (int i = 0; i < number; i++) {
-			if(i%3 == 0 || i%5 == 0) {
+		for (int i = 0; i < maximumNumber; i++) {
+			if(shouldWeAddCurrentInteger(i, multiplesToConsider)) {
 				sum+= i;
 			}
 		}
 		return sum;
 	}
-
+	
+	private boolean shouldWeAddCurrentInteger(int currentInteger, int[] multiplesToConsider) {
+		boolean b = false;
+		for (int currentMultiple : multiplesToConsider) {
+			if (currentInteger % currentMultiple == 0) { // if at least one is true, we add currentNumber to sum
+				b = true;
+			}
+		}
+		return b;
+	}
 }
