0cee8f056167f096f52f57107a1fa537dc41c373
tmi <michelet.tristan@gmail.com>
2013-01-07 19:11:35 +0100

    to go further with problem 1

--- src/Problem1.java
+++ src/Problem1.java
@@ -10,16 +10,18 @@ public class Problem1 {
 
 	  Find the sum of all the multiples of 3 or 5 below 1000.
 	 * */
-	
+
 	@Test
 	public void test() {
+		assertEquals(0, natural35Sum(0));
+		assertEquals(0, natural35Sum(2));
 		assertEquals(23, natural35Sum(10));
 		assertEquals(14, natural35Sum(9));
 		assertEquals(33, natural35Sum(11));
 		assertEquals(233168, natural35Sum(1000));
 	}
 
-	
+
 	// easy case with 3 and 5
 	private int natural35Sum(int maximumNumber) {
 		int sum = 0;
@@ -34,26 +36,37 @@ public class Problem1 {
 	private boolean shouldWeAddCurrentInteger(int i) {
 		return i%3 == 0 || i%5 == 0;
 	}
-	
-	
 
-	
-	
+
+
+
+
 	// to go further
-	
+
 	@Test
-	public void furtherTest() {
+	public void furtherTest() throws Exception {
 		int[] multiples = new int[]{3,5};
+		assertEquals(0, naturalSum(0, multiples));
 		assertEquals(23, naturalSum(10, multiples));
 		assertEquals(14, naturalSum(9, multiples));
 		assertEquals(33, naturalSum(11, multiples));
 		assertEquals(233168, naturalSum(1000, multiples));
-	}
-
-	
-	private int naturalSum(int maximumNumber, int[] multiplesToConsider) {
-		int sum = 0;
 		
+		multiples = new int[]{7};
+		assertEquals(0, naturalSum(0, multiples));
+		assertEquals(0, naturalSum(6, multiples));
+		assertEquals(7, naturalSum(10, multiples));
+		assertEquals(71071, naturalSum(1000, multiples));
+		
+	}
+	
+
+
+	private int naturalSum(int maximumNumber, int[] multiplesToConsider) throws Exception {
+		chekcInput(maximumNumber, multiplesToConsider);
+
+		int sum = 0;
+
 		for (int i = 0; i < maximumNumber; i++) {
 			if(shouldWeAddCurrentInteger(i, multiplesToConsider)) {
 				sum+= i;
@@ -61,7 +74,8 @@ public class Problem1 {
 		}
 		return sum;
 	}
-	
+
+
 	private boolean shouldWeAddCurrentInteger(int currentInteger, int[] multiplesToConsider) {
 		boolean b = false;
 		for (int currentMultiple : multiplesToConsider) {
@@ -71,4 +85,54 @@ public class Problem1 {
 		}
 		return b;
 	}
+
+	
+	
+	
+	
+	
+	private static String MAX_SHOULD_NOT_BE_NEGATIVE = "maximumNumber should not be negative";
+	private static String MULTIPLES_SHOULD_NOT_BE_NULL = "multiplesToConsider should not be null";
+	private static String MULTIPLES_SHOULD_NOT_BE_EMPTY = "multiplesToConsider should not be empty";
+	private static String MULTIPLES_SHOULD_CONTAIN_ONLY_POSITIVE_VALUES = "multiplesToConsider should contain only positive values";
+
+	@Test
+	public void furtherTest_chekcInput() throws Exception {
+
+		testCheckInputWith(-5, new int[]{7}, MAX_SHOULD_NOT_BE_NEGATIVE);
+		testCheckInputWith(5, null, MULTIPLES_SHOULD_NOT_BE_NULL);
+		testCheckInputWith(5, new int[]{}, MULTIPLES_SHOULD_NOT_BE_EMPTY);
+		testCheckInputWith(5, new int[]{7, 0}, MULTIPLES_SHOULD_CONTAIN_ONLY_POSITIVE_VALUES);
+		testCheckInputWith(5, new int[]{7, -5}, MULTIPLES_SHOULD_CONTAIN_ONLY_POSITIVE_VALUES);
+	}
+
+
+	private void testCheckInputWith(int max, int[] multiples, String expectedExceptionMessage) {
+		try {
+			naturalSum(max, multiples);
+			fail();
+		}
+		catch(Exception e) {
+			assertEquals(expectedExceptionMessage, e.getMessage());
+		}
+	}
+	
+	
+	private void chekcInput(int maximumNumber, int[] multiplesToConsider) throws Exception {
+		if (maximumNumber < 0) {
+			throw new Exception(MAX_SHOULD_NOT_BE_NEGATIVE);
+		}
+		if (multiplesToConsider == null) {
+			throw new Exception(MULTIPLES_SHOULD_NOT_BE_NULL);
+		}
+		if (multiplesToConsider.length == 0) {
+			throw new Exception(MULTIPLES_SHOULD_NOT_BE_EMPTY);
+		}
+		for (int i : multiplesToConsider) {
+			if (i <= 0) {
+				throw new Exception(MULTIPLES_SHOULD_CONTAIN_ONLY_POSITIVE_VALUES);
+			}
+		}
+	}
 }
+
