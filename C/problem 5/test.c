5630b1759d24d6e91cb38bdeba6873ee0cbba872
tmi <michelet.tristan@gmail.com>
2013-01-16 23:29:01 +0100

    better smart way for problem 5

--- C/problem 5/test.c
+++ C/problem 5/test.c
@@ -38,38 +38,43 @@ int smallestDivisible_bruteForce(int number) {
 
 // to go further
 
-int smallestDivisible(int number) {
-    if (number < 1) {
+/*
+    We are going to divide each number from 2 to maxNumber into it's prime factors. 
+    We are going to save the highest power value needed for each prime factor.
+ */
+
+int smallestDivisible(int maxNumber) {
+    if (maxNumber < 1) {
         return 0; //TODO throw exception
     }
-    if (number == 1) {
+    if (maxNumber == 1) {
         return 1;
     }
     
     int* dividers = (int *) malloc(sizeof(int));
-    for (int i = 0; i <= number; i++) {
-        dividers[i] = 0; // 0 to number included
-    }
+    for (int i = 0; i <= maxNumber; i++) {
+        dividers[i] = 0; // considering prime dividers from 0 to number included
+    } // we could optimize the memory by considering only prime numbers.
     
     
-    for (int currentNumber = 2; currentNumber <= number; currentNumber ++) {
-        // decompose current number into prime numbers, and add them to the dividers table  
+    for (int currentNumber = 2; currentNumber <= maxNumber; currentNumber ++) { // consider each number under maxNumber
         int theNumber = currentNumber;
-        for (int prime = 2; prime <= theNumber; prime++) {
-            int count = 0;
+        
+        for (int prime = 2; prime <= theNumber; prime++) { // find it's prime dividers
+            int count = 0; // count how many times they are needed
             while(theNumber % prime == 0) {
                 count ++;
                 theNumber = theNumber / prime;
             }
-            if (dividers[prime] < count) {
+            if (dividers[prime] < count) { // If we don't have them already, add them to the divider table
                 dividers[prime] = count;
             }
         }
     }
     
     int result = 1;
-    for (int i = 2; i <= number; i++) {
-        result *= pow(i, dividers[i]);;
+    for (int i = 2; i <= maxNumber; i++) {
+        result *= pow(i, dividers[i]);
     }
 
     
